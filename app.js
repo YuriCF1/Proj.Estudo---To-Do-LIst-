c215997133906a02deaa771ea6b0bf6e449634a8
Yuri Cruz <yurifdev@gmail.com>
2022-05-29 00:18:39 -0300

    Criado banco de dados e adiciona card

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// <label class="todo__item">
+// <input type="checkbox">
+// <div>teste de item 2</div>
+// <input type="button" value="X">
+// </label> 
+
+let banco = [
+    {'tarefa':'Estudar JS', 'status': ''},
+    {'tarefa':'Netflix', 'status': 'checked'}
+
+
+];
+
+const criarItem = (tarefa, status='') => { // Se alguém não passa informação nenhuma, é pq tá vazio
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status}=''>
+        <div>${tarefa}</div>
+        <input type="button" value="X">
+
+    `
+    
+    document.getElementById('todoList').appendChild(item);
+
+ 
+}
+
+const atualizarTela = () => {//Normalmente se coloca 'render', pois rendeiza na tela
+    banco.forEach(item => criarItem(item.tarefa)); 
+
+} 
+
+atualizarTela();
